08406de4eb2c81e1859db0fa5d3006702edbfe95
Vicky Gupta <vickyguptaa7@gmail.com>
2022-03-12 20:19:42 +0530

    solved clone list

--- 5.LinkedList/Clone_Linked_List.cpp
+++ 5.LinkedList/Clone_Linked_List.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 struct node
 {
@@ -9,7 +10,7 @@ struct node
     {
         data = val;
         next = nullptr;
-        random=nullptr;
+        random = nullptr;
     }
 };
 
@@ -72,18 +73,73 @@ int Count_Node(node *head)
     }
     return count;
 }
-node*Reverse(node*head)
+node *Reverse(node *head)
 {
-    if(!head||!head->next)return head;
-    node*prev=nullptr,*curr=head,*next=nullptr;
-    while(curr)
+    if (!head || !head->next)
+        return head;
+    node *prev = nullptr, *curr = head, *next = nullptr;
+    while (curr)
     {
-        next=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=next;
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
     }
     return prev;
+}
+
+node *Clone_Linked_List(node *head) // T.C ->  O(n) and S.C -> O(n) bcoz of map
+{
+    unordered_map<node *, node *> umap;
+    node *curr = head;
+    while (curr)
+    {
+        node *newnode = new node(curr->data);
+        umap[curr] = newnode;
+        curr = curr->next;
+    }
+    curr = head;
+    while (curr) // umap[nullptr] is zero so we dont need to explicity set it
+    {
+        umap[curr]->next = umap[curr->next];
+        umap[curr]->random = umap[curr->random];
+        curr = curr->next;
+    }
+    return umap[head];
+}
+
+node *Clone_Linked_List_Opt(node *head) // T.C ->  O(n) and S.C -> O(n) bcoz of map
+{
+    if (!head)
+        return nullptr;
+    node *curr = head;
+    while (curr) // created nodes
+    {
+        node *newNode = new node(curr->data);
+        newNode->next = curr->next;
+        curr->next = newNode;
+        curr = curr->next->next;
+    }
+    curr = head;
+    while (curr) /// assigned random pointers
+    {
+        if (curr->random)
+            curr->next->random = curr->random->next;
+        else
+            curr->next->random = nullptr;
+        curr = curr->next->next;
+    }
+    node *newHead = head->next;
+    curr = newHead;
+    while (curr && curr->next) // assigned next pointers
+    {
+        head->next = curr->next;
+        curr->next = curr->next->next;
+        curr = curr->next;
+        head = head->next;
+    }
+    head->next = nullptr;
+    return newHead;
 }
 
 int main()
@@ -97,8 +153,8 @@ int main()
     InsertAtTail(head, 6);
     InsertAtTail(head, 2);
     InsertAtTail(head, 3);
-    
 
+    Display(Clone_Linked_List_Opt(head));
     Display(head);
 
     return 0;
